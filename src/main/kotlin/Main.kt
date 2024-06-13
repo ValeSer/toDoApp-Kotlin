518c4082150114c6d1bd8b2ddfb62d5304b6d0fa
Valeria Serci <balerty84@gmail.com>
2024-06-13 21:43:32 +0100

    add todo list menu with 4 options

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
-fun main(args: Array<String>) {
-    println("Welcome!")
+fun main() {
+    val todoList = mutableListOf<String>()
+    while (true) {
+        println("\nTo-Do List Application")
+        println("1. View To-Do List")
+        println("2. Add Item")
+        println("3. Remove Item")
+        println("4. Exit")
+        print("Choose an option: ")
+
+        when (readlnOrNull()?.toIntOrNull()) {
+            1 -> viewTodoList(todoList)
+            2 -> addItem(todoList)
+            3 -> removeItem(todoList)
+            4 -> {
+                println("Exiting...")
+                return
+            }
+            else -> println("Invalid option. Please try again.")
+        }
+    }
 }
+
